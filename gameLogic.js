d17f058d14065a950122d6b406490f9c327ace0d
Dakota Mumford <dakotamum@gmail.com>
2024-01-07 21:55:06 -0700

    initial maze size selection works

--- gameLogic.js
+++ gameLogic.js
@@ -4,24 +4,31 @@ TODO:
 */
 
 MazeGame.main = (function (graphics) {
-  let start = {};
   ("use strict");
+  let start = {};
 
   let lastFrameTime = performance.now();
   let dim = Array.from(document.getElementsByName("maze-dimensions")).find(
     (radio) => radio.checked
   ).value;
-  console.log(dim);
-  let maze = new Maze(512, dim, dim);
-  maze.initialize();
-  let cellSize = Math.floor(maze.size / maze.rows);
-  let player = new Player(
-    cellSize / 2,
-    cellSize / 2,
-    cellSize / 3,
-    "magenta",
-    maze
-  );
+
+  function handleDimensionsOptionChange(event) {
+    dim = event.target.value;
+  }
+
+  let option4by4 = document.getElementById("4x4");
+  let option8by8 = document.getElementById("8x8");
+  let option16by16 = document.getElementById("16x16");
+  let option32by32 = document.getElementById("32x32");
+
+  option4by4.addEventListener("change", handleDimensionsOptionChange);
+  option8by8.addEventListener("change", handleDimensionsOptionChange);
+  option16by16.addEventListener("change", handleDimensionsOptionChange);
+  option32by32.addEventListener("change", handleDimensionsOptionChange);
+
+  let maze;
+  let player;
+  let cellSize;
 
   function render() {
     graphics.drawPlayer(player);
@@ -41,6 +48,16 @@ MazeGame.main = (function (graphics) {
   }
 
   start.initialize = function () {
+    maze = new Maze(512, dim, dim);
+    maze.initialize();
+    cellSize = Math.floor(maze.size / maze.rows);
+    player = new Player(
+      cellSize / 2,
+      cellSize / 2,
+      cellSize / 3,
+      "magenta",
+      maze
+    );
     graphics.drawMaze(maze);
     requestAnimationFrame(gameLoop);
   };
